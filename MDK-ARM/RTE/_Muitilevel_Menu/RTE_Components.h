e1b4c181c7cd4a76de572d61c5a2bf16d268a1a3
Star <dingyi233@mails.ucas.ac.cn>
2024-06-11 01:10:36 +0800

    docs(*): change file names

--- MDK-ARM/RTE/_Muitilevel_Menu/RTE_Components.h
+++ MDK-ARM/RTE/_Muitilevel_Menu/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Muitilevel_Menu' 
+ * Project: 'MuMeStar' 
  * Target:  'Muitilevel_Menu' 
  */
 
